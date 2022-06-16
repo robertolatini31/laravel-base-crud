f0b8c79b5f6857a02b9bda8192a5045458dcdb4b
robertolatini31 <robertolatini31@gmail.com>
2022-06-16 15:31:23 +0200

    add comics view

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('page_title')
+Comics
+@endsection
+
+@section('content')
+
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>title</th>
+                <th>thumb</th>
+                <th>price</th>
+                <th>series</th>
+                <th>sale_date</th>
+                <th>type</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($comics as $comic)
+                <tr>
+                    <td scope="row">{{$comic->id}}</td>
+                    <td>{{$comic->title}}</td>
+                    <td><img width="50" src="{{$comic->thumb}}" alt="/"></td>
+                    <td>{{$comic->price}} &euro;</td>
+                    <td>{{$comic->series}}</td>
+                    <td>{{$comic->sale_date}}</td>
+                    <td>{{$comic->type}}</td>
+                </tr>
+            @empty
+                <td>No Comics Yet!</td>
+            @endforelse
+        </tbody>
+    </table>
+    
+</div>
+
+@endsection
